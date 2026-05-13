6a1630de5b98776c33c1d75a5d6c2e0dc6bc0d08
gaitano <dev.gaitano@gmail.com>
2026-05-13 11:02:32 +0300

    feat(ProductCard): add ProductCard component
    - Create "ProductCard.jsx" to components dir
    - Add base element structure

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,25 @@
+function ProductCard() {
+  return (
+    <div>
+      <div>
+        <img src="" alt="product-img" />
+      </div>
+      <div>
+        <div>
+          <p></p>
+          <div>
+            <img src="" alt="star" />
+            <p></p>
+          </div>
+        </div>
+        <div></div>
+        <div>
+          <p></p>
+          <button type=""></button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductCard
