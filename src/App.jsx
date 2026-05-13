7aeb68fde2e5a67a935dfe84edf90c0705732dc7
gaitano <dev.gaitano@gmail.com>
2026-05-13 11:52:48 +0300

    fix(naming): change productCard to productList

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import './App.css'
+import ProductList from './components/ProductList'
 
-function App() {}
+function App() {
+  return (
+    <>
+      <ProductList />
+    </>
+  )
+}
 
 export default App
