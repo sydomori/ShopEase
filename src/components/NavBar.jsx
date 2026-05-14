7d83326390eab683f33a823a9dbf229532e94d1b
sydomori <sydneyomori@gmail.com>
2026-05-14 13:47:46 +0300

    style: add search section styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,17 +6,13 @@ function NavBar() {
         <nav className="navbar">
             <div className="top-section">
                 <h1 className="logo">ShopEase</h1>
-                <>
-                    <input className='searchBar' type="text" placeholder="Search products..." />
-                    <button onClick={() => console.log('Search clicked')}>Search</button>
-                </>
-                
-                <button className="cart-button" onClick={() => console.log('Cart clicked')}>
-                    View Cart
-                </button>
-            
             </div>
+
             <div className="form-section">
+                <div className="search-section">
+                    <input className='form-control searchBar' type="text" placeholder="Search products..." />
+                    <button className='btn btn-primary' onClick={() => console.log('Search clicked')}>Search</button>
+                </div>
                 <AddProductForm />
             </div>
         </nav>
