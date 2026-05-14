3b5286da52d8afeb10c69bd9ffedbb794c8f112a
sydomori <sydneyomori@gmail.com>
2026-05-14 23:46:56 +0300

    style: add style to dashboard to create orientation

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -5,11 +5,15 @@ function ItemCard({ name, price, image, category }) {
   return (
     <div className="item-card">
       <img src={image} alt={name} />
-      <h3>{name}</h3>
-      <p>${price}</p>
-      <p>{category}</p>
-      <button>Edit</button>
-      <button>Delete</button>
+      <div className='card-content'>
+        <h3>{name}</h3>
+        <p>${price}</p>
+        <p>{category}</p>
+      </div>
+      <div className='button-grp'>
+        <button type='button' className='btn btn-primary'>Edit</button>
+        <button type='button' className='btn btn-danger'>Delete</button>
+      </div>
     </div>
   );
 }
