190c80d459ad9e084fd7c1a88380ee7763182e4a
sydomori <sydneyomori@gmail.com>
2026-05-14 13:34:00 +0300

    feat: add bootstrap link for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Dashboard from './components/Dashboard'
-import AddProductForm from './components/AddProductForm'
 import './App.css'
 
 function App() {
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,16 @@
 import '../styles/NavBar.css';
-import AddProductForm from './AddProductForm';
+import AddProductForm from './addProductForm';
 
 function NavBar() {
     return (
         <nav className="navbar">
             <div className="top-section">
-            <h1 className="logo">ShopEase</h1>
-            <div className="search-bar">
-                <input type="text" placeholder="Search products..." />
-                <button onClick={() => console.log('Search clicked')}>Search</button>
-            </div>
-            
+                <h1 className="logo">ShopEase</h1>
+                <>
+                    <input className='searchBar' type="text" placeholder="Search products..." />
+                    <button onClick={() => console.log('Search clicked')}>Search</button>
+                </>
+                
                 <button className="cart-button" onClick={() => console.log('Cart clicked')}>
                     View Cart
                 </button>
