19991735712330a13dbb22442f43f11235cd48b4
sydomori <sydneyomori@gmail.com>
2026-05-15 04:53:03 +0300

    style: add style to edit form

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -13,10 +13,10 @@ function ItemCard({ id,title, price, thumbnail, category, onEditProduct }) {
       <img src={thumbnail} alt={title} />
       {
         isEditing ? (
-          <div>
-            <input type="text" value={editedTitle} onChange={(e) => setEditedTitle(e.target.value)} />
-            <input type="number" value={editedPrice} onChange={(e) => setEditedPrice(e.target.value)} />
-            <input type="text" value={editedCategory} onChange={(e) => setEditedCategory(e.target.value)} />
+          <div className="d-flex flex-column gap-3">
+            <input className="form-control" type="text" value={editedTitle} onChange={(e) => setEditedTitle(e.target.value)} />
+            <input className="form-control" type="number" value={editedPrice} onChange={(e) => setEditedPrice(e.target.value)} />
+            <input className="form-control" type="text" value={editedCategory} onChange={(e) => setEditedCategory(e.target.value)} />
             <button type="button" className="btn btn-primary" onClick={()=>{onEditProduct(id,{title: editedTitle, price: editedPrice, category: editedCategory}); setIsEditing(false)}}>Save</button>
           </div>
         ) : (
