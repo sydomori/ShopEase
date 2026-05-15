8d7905b0fd7c8cd9886f4cba09dbc11473d3928d
sydomori <sydneyomori@gmail.com>
2026-05-15 04:57:52 +0300

    feat: add onDeleteProduct props to Dashboard and Productlist functions

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,16 +2,16 @@ import ItemCard from "./ItemCard";
 import '../styles/Global.css';
 import '../styles/Dashboard.css';
 
-function Dashboard({products, onEditProduct}) {
+function Dashboard({products, onEditProduct, onDeleteProduct}) {
   return (
     <div className="dashboard">
       <h2>Your Products</h2>
-      <ProductList products={products} onEditProduct={onEditProduct} />
+      <ProductList products={products} onEditProduct={onEditProduct} onDeleteProduct={onDeleteProduct} />
     </div>
   );
 }
 
-function ProductList({products, onEditProduct}) {
+function ProductList({products, onEditProduct, onDeleteProduct}) {
     return (
         <div className="item-grid">
             {products.map((product) => (
